f8502b1ba2e65066c3f6a49fd3541ab6a6e8c686
Mohamedanter845 <mohamedanter845@gmail.com>
2025-10-04 17:51:56 +0300

    Fix Prettier formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
-import prettier from "eslint-config-prettier";
+import js from '@eslint/js';
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
 
 export default [
-  
   js.configs.recommended,
   prettier,
 
- 
   {
     languageOptions: {
       globals: {
-        ...globals.node,      
-        ...globals.browser,  
-        ...globals.jest,      
+        ...globals.node,
+        ...globals.browser,
+        ...globals.jest,
       },
     },
     rules: {
-      semi: "error",
-      quotes: ["error", "double"],
+      semi: 'error',
+      quotes: ['error', 'double'],
     },
   },
 ];
-
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,9 +1,9 @@
-const request = require("supertest");
-const app = require("../server");
+const request = require('supertest');
+const app = require('../server');
 
-describe("Smoke test", () => {
-  test("GET / should return HTML", async () => {
-    const res = await request(app).get("/");
+describe('Smoke test', () => {
+  test('GET / should return HTML', async () => {
+    const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
     expect(res.text).toMatch(/Team Availability/);
   });
